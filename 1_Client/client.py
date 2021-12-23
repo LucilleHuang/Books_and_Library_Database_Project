667da7ee1d8268e5c3e5b8e81f8c55a298afdfad
Shang Jian Deng <tom.deng9701@gmail.com>
2021-12-23 12:18:27 -0500

    added checkout

--- 1_Client/client.py
+++ 1_Client/client.py
@@ -1,5 +1,7 @@
 import argparse
 import mysql.connector
+from datetime import date, datetime, timedelta
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("command")
@@ -7,14 +9,20 @@ parser.add_argument("-t", "--title")
 parser.add_argument("-a", "--author")
 parser.add_argument("-s", "--subject")
 #parser.add_argument("-r", "--rating")
-#parser.add_argument("-n", "--number")
+
+parser.add_argument("-n", "--number")
+parser.add_argument("-b", "--barcode")
+parser.add_argument("-i", "--type")
+parser.add_argument("-c", "--collection")
+parser.add_argument("-l", "--callnumber")
+
 args = parser.parse_args()
 
 cnx = mysql.connector.connect(
     host="marmoset04.shoshin.uwaterloo.ca",
-    user="",
-    password="",
-    database=""
+    user="j349huan",
+    password="123Abc!!",
+    database="db356_j349huan"
 )
 
 #test query
@@ -36,6 +44,8 @@ if (args.command == "Search"):
 
     if (args.title or args.author or args.subject):
         query = query + " WHERE"
+    else:
+        sys.exit("Must specity either a title, author, or subject")
 
     if (args.title):
         query = query + " TITLE LIKE '%" + args.title + "%'"
@@ -67,9 +77,20 @@ if (args.command == "Search"):
         print("\n")
 
 
-elif (args.command == "CheckStatus"):
-    print("Check Status")
-    #TODO
+elif (args.command == "Checkout"):
+    query = "INSERT INTO LibraryCheckout"
+    query +=  " (Bibnumber, ItemBarcode, ItemType, Collection, CallNumber, CheckoutDateTime)"
+    query += " VALUE (%s, %s, %s, %s, %s, %s)"
+
+    if (not(args.number and args.barcode and args.type and args.collection and args.callnumber)):
+        sys.exit("Must specify Bibnumber, Barcode, Type, Collection, and Number")
+
+    checkout = (args.number, args.barcode, args.type, args.collection, args.callnumber, datetime.now())
+
+    cursor.execute(query, checkout)
+
+    cnx.commit()
+
 elif (args.command == "UpdateStatus"):
     print("Update Status")
     #TODO
